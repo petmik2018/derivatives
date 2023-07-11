49c39516c3cb69c12b3bc6203227bbf49d050397
petmik2018 <petmik@yandex.ru>
2023-07-11 21:38:40 +0300

    comments added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# derivatives
+# derivatives methods
+Here is the comparison of 1-order differentiation numerical methods:
+1. numpy.diff
+2. numpy gradient
+3. 3-points differentiation
+4. 5-points differentiation
+5. finite elements method +conjugate approximation
+
+the results of the calculations are compared with the "exact" values
+of the analitic derivative. You can choose the test function as you like. 
--- main.py
+++ main.py
@@ -7,32 +7,33 @@ float_formatter = "{:.3f}".format
 np.set_printoptions(formatter={'float_kind': float_formatter})
 
 
-def test_function(x):
+def test_function(x):  # function to be differentiated
     # return np.exp(x)
     return np.sin(x)
 
 
-def test_derivative(x):
+def test_derivative(x):  # analitic derivative of tested function
+    # return np.exp(x)
     return np.cos(x)
 
-
+# parameters of arguments set = array[0, ..., n+1]
 xMin = 0
 # xMax = 1.0
 xMax = np.pi / 2
-
 n = 20
+
 x = np.linspace(xMin, xMax, n+1)
 y = test_function(x)
 dydx = test_derivative(x)
 
 sourceFunction = DiscreteFunction(x, y)
-derivative_diff = sourceFunction.numpy_diff()
-derivative_gradient = sourceFunction.numpy_gradient()
-derivative_3_points = sourceFunction.points_3(mode="valid")
-derivative_5_points = sourceFunction.points_5()
-approx_fem = sourceFunction.approximation()
+derivative_diff = sourceFunction.numpy_diff()  # numpy.diff method
+derivative_gradient = sourceFunction.numpy_gradient()  # numpy.gradient method
+derivative_3_points = sourceFunction.points_3()  # 3-points approximation, possible parameter mode='same'
+derivative_5_points = sourceFunction.points_5()  # 3-points approximation, possible parameter mode='same'
+approx_fem = sourceFunction.approximation()  # finite elements method + congjugate approximation
 
-plt.xlim(0.0, 0.2)
+plt.xlim(0.0, 0.2)  # window sizes for the plots
 plt.ylim(0.977, 1.001)
 
 plt.plot(derivative_diff.arguments, derivative_diff.values, color='blue', linewidth=2, label="np.diff")
@@ -41,6 +42,9 @@ plt.plot(derivative_3_points.arguments, derivative_3_points.values, color='yello
 plt.plot(derivative_5_points.arguments, derivative_5_points.values, color='violet', linewidth=2, label="5-points")
 plt.plot(approx_fem.arguments, approx_fem.values, color='red', linewidth=2, label="finite elements")
 plt.plot(x, dydx, color='green', linewidth=2, label="test")
+
+# if you cannot see approx_fem plot, comment dydx plot
+# if yu cannot see 5-points plot, comment approx_fem and dydx plots
 
 plt.legend()
 plt.show()
